4201f6c7f613b2945b8f9a48543c0c01b0e844a5
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-14 17:44:51 -0700

    WIP

--- dist/middleware/validateOrganization.js
+++ dist/middleware/validateOrganization.js
@@ -8,13 +8,12 @@ function validateOrganization(req, res, next) {
   var organization = req.params.organization;
   var payload = req.body.payload;
 
-
-  console.log('req.body', req.body);
-  console.log('payload', payload);
+  var _JSON$parse = JSON.parse(payload),
+      login = _JSON$parse.organization.login;
 
   if (!organization) {
-    res.status(401).send('\n\n      Error! The organization provided is null.\n\n      Please set webhook url to https://webhook.gittoken.io/' + JSON.parse(payload).organization + '\n\n    ');
-  }if (organization != payload.organization) {
+    res.status(401).send('\n\n      Error! The organization provided is null.\n\n      Please set webhook url to https://webhook.gittoken.io/' + login + '\n\n    ');
+  }if (organization != login) {
     res.status(401).send('\n\n      Error! The organization provided does not match the webhook organization login.\n\n    ');
   } else {
     this.selectFromRegistry({
--- src/middleware/validateOrganization.js
+++ src/middleware/validateOrganization.js
@@ -1,9 +1,7 @@
 export default function validateOrganization(req, res, next) {
   const { organization } = req.params
   const { payload } = req.body
-
-  console.log('req.body', req.body)
-  console.log('payload', payload)
+  const { organization: { login } } = JSON.parse(payload)
 
 
   if (!organization) {
@@ -11,10 +9,10 @@ export default function validateOrganization(req, res, next) {
 
       Error! The organization provided is null.
 
-      Please set webhook url to https://webhook.gittoken.io/${JSON.parse(payload).organization}
+      Please set webhook url to https://webhook.gittoken.io/${login}
 
     `)
-  } if (organization != payload.organization) {
+  } if (organization != login) {
     res.status(401).send(`
 
       Error! The organization provided does not match the webhook organization login.
